25bd45ace39405c25ae83c43e1648a5a9fbd0d36
Rafiul901 <rafiuldipu2480@gmail.com>
2025-03-02 21:27:50 +0600

    new

--- java/main.js
+++ java/main.js
@@ -3,10 +3,10 @@ const btnAll = document.getElementsByClassName("btn-all")
 
 for (const element of btnAll) {
     element.addEventListener('click',(event)=>{
-        alert("Board updated Successfully!")
+        alert("Board updated Successfully")
 
         if (btnAll.length===1) {
-            alert("Congrats! You have completed all the current task")
+            alert("Congrats!!! You have completed all the current task")
         }
         event.target.classList.remove("btn-all")
         event.target.setAttribute('disabled','')
